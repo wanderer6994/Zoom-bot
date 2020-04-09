8422e360d47bc6d5fa7b5b6e501dca4a80ae60d5
Sudarshan <senthilsudarshan@gmail.com>
2020-04-10 03:17:59 +0530

    [ssk] cleanup

--- zoom_bot_prod.py
+++ zoom_bot_prod.py
@@ -3,21 +3,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from datetime import datetime
 import time
 import sys
 
+
 #------------------------------------ ENTER YOUR INFO HERE ---------------------------------------------------
-USERNAME = ''
-PASSWORD = ''
+USERNAME = ''   #zoom username
+PASSWORD = '' #zoom password
 NAME = "" #meeting name
-zoom_key = ''
-zoom_password = ""
+zoom_key = ''   #meeting_id
+zoom_password = "" #meeting_password (leave blank if no password is required)
+meeting_time = "" #in HH:MM:SS 24 hour clock
 operating_system = 'mac' # mac, windows, linux
+
 #------------------------------------ ADVANCED SETTINGS ---------------------------------------------------
 retry_time = 1
 retry_time *= 60
 
 #-------------------------------------------------------------------------------------------------------------
+
+#initialising drivers
 driver = None
 if(operating_system == 'mac'):
     driver = webdriver.Chrome("./drivers/chromedriver_mac")
@@ -39,35 +45,28 @@ def check_if_element_exists_id(id):
     except:
         return False
 
-#join meeting at a later time if it hasn't started yet
-def join_after_signin():
+def check_if_element_exists_class(name):
     try:
-        driver.get(login_url)
-        delay = 3
-        try:
-            WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'inputname')))
-            print("Join meeting page loaded")
-        except TimeoutException:
-            print("Loading took too much time!")
+        driver.find_element_by_class_name(name)
+        return True
+    except:
+        return False
 
-        input_meeting_name = driver.find_element_by_id('inputname')
-        input_meeting_name.clear()
-        input_meeting_name.send_keys(NAME)
-        driver.find_element_by_id('joinBtn').click()
+def wait_for_element_to_load_name(name):
+    delay = 3  # seconds
+    try:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, name)))
+        print("Page Loaded!")
+    except TimeoutException:
+        print("Loading took too much time!")
 
-        delay = 3  # seconds
-        try:
-            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'inputname')))
-            print("Join meeting page loaded")
-        except TimeoutException:
-            print("Loading took too much time!")
-
-        driver.find_element_by_class_name('zm-btn--lg').click()
-
-        driver.switch_to.alert().accept()
-    except Exception as e:
-        raise e
-
+def wait_for_element_to_load_class(name):
+    delay = 3  # seconds
+    try:
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, name)))
+        print("Page Loaded!")
+    except TimeoutException:
+        print("Loading took too much time!")
 
 def login_and_join():
     try:
@@ -77,12 +76,8 @@ def login_and_join():
         driver.find_element_by_xpath('//div[@class="signin"]').click()
 
         delay = 3  # seconds
-        try:
-            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'inputname')))
-            print("Join meeting page loaded")
-        except TimeoutException:
-            print("Loading took too much time!")
 
+        wait_for_element_to_load_name('inputname')
         input_meeting_name = driver.find_element_by_id('inputname')
         input_meeting_name.clear()
         input_meeting_name.send_keys(NAME)
@@ -92,27 +87,18 @@ def login_and_join():
 
         driver.find_element_by_id('joinBtn').click()
 
-        try:
-            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'inputname')))
-            print("Join meeting page loaded")
-        except TimeoutException:
-            print("Loading took too much time!")
-
+        wait_for_element_to_load_name('inputname')
         driver.find_element_by_class_name('zm-btn--lg').click()
 
-        try:
-            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'tab-button')))
-            print("Joined meeting successfully!")
-        except TimeoutException:
-            print("Loading took too much time!")
+        wait_for_element_to_load_class('tab-button')
         driver.find_element_by_class_name('tab-button').click()
 
         driver.switch_to.alert().accept()
     except Exception as e:
-        print("Failed with error: " +str(e))
-        print("Retrying in " +str(retry_time) +' seconds.')
-        while True:
+        while not check_if_element_exists_class('footer__leave-btn'):
+            print("Failed with error: " + str(e))
+            print("Retrying in " + str(retry_time) + ' seconds.')
             time.sleep(retry_time)
-            join_after_signin()
+            login_and_join()
 
 login_and_join()
