11e166b962ed2fc1d0dbfa6ec7fdcb49475a5a5f
Sudarshan <senthilsudarshan@gmail.com>
2020-04-11 04:26:26 +0530

    [ssk] minutes fix

--- zoom_bot_prod.py
+++ zoom_bot_prod.py
@@ -7,23 +7,22 @@ from datetime import datetime
 import time
 import sys
 
+# ------------------------------------ ENTER YOUR INFO HERE ---------------------------------------------------
+USERNAME = ''  # zoom username
+PASSWORD = ''  # zoom password
+NAME = ""  # meeting name
+zoom_key = ''  # meeting_id
+zoom_password = ""  # meeting_password (leave blank if no password is required)
+meeting_time = ""  # in HH:MM:SS 24 hour clock
+operating_system = 'mac'  # mac, windows, linux
 
-#------------------------------------ ENTER YOUR INFO HERE ---------------------------------------------------
-USERNAME = ''   #zoom username
-PASSWORD = '' #zoom password
-NAME = "" #meeting name
-zoom_key = ''   #meeting_id
-zoom_password = "" #meeting_password (leave blank if no password is required)
-meeting_time = "" #in HH:MM:SS 24 hour clock
-operating_system = 'mac' # mac, windows, linux
-
-#------------------------------------ ADVANCED SETTINGS ---------------------------------------------------
+# ------------------------------------ ADVANCED SETTINGS ---------------------------------------------------
 retry_time = 1  # Time before checking if element is present again
 
-sleep_time = 1 # Time after failure before bot tries joining the meeting again
-#-------------------------------------------------------------------------------------------------------------
+sleep_time = 1  # Time after failure before bot tries joining the meeting again
+# -------------------------------------------------------------------------------------------------------------
 
-#initialising drivers
+# initialising drivers
 driver = None
 if operating_system == 'mac':
     driver = webdriver.Chrome("./drivers/chromedriver_mac")
@@ -35,15 +34,16 @@ else:
     print("OS Not chosen. Please check and try again.")
     sys.exit(0)
 
-login_url = 'https://zoom.us/wc/join/' +zoom_key +'?wpk='
+login_url = 'https://zoom.us/wc/join/' + zoom_key + '?wpk='
 
 # Extracting meeting hour, minutes and seconds
 meeting_hour = int(meeting_time[0:2])
 meeting_minute = int(meeting_time[3:5])
 meeting_second = int(meeting_time[6:8])
-print("meeting time: " +str(meeting_hour) +" " +str(meeting_minute) + " " +str(meeting_second))
+print("meeting time: " + str(meeting_hour) + " " + str(meeting_minute) + " " + str(meeting_second))
 
-#checks if an element is present on webpage by id
+
+# checks if an element is present on webpage by id
 def check_if_element_exists_id(id):
     try:
         driver.find_element_by_id(id)
@@ -51,12 +51,14 @@ def check_if_element_exists_id(id):
     except:
         return False
 
+
 def check_if_element_exists_class(name):
     try:
         driver.find_element_by_class_name(name)
         return True
     except:
         return False
+
 
 def wait_for_element_to_load_name(name):
     delay = 3  # seconds
@@ -66,6 +68,7 @@ def wait_for_element_to_load_name(name):
     except TimeoutException:
         print("Loading took too much time!")
 
+
 def wait_for_element_to_load_class(name):
     delay = 3  # seconds
     try:
@@ -73,6 +76,7 @@ def wait_for_element_to_load_class(name):
         print("Page Loaded!")
     except TimeoutException:
         print("Loading took too much time!")
+
 
 def login_and_join():
     try:
@@ -104,15 +108,16 @@ def login_and_join():
         while not check_if_element_exists_class('footer__leave-btn'):
             print("Failed with error: " + str(e))
             print("Retrying in " + str(retry_time) + ' seconds.')
-            time.sleep(retry_time*60)
+            time.sleep(retry_time * 60)
+
 
 while True:
     now = datetime.now()
-    print("now time: " +str(now.hour) +" " +str(now.minute))
-    if now.hour == meeting_hour and now.minute >= meeting_second:
+    print("now time: " + str(now.hour) + " " + str(now.minute))
+    if now.hour == meeting_hour and now.minute >= meeting_minute:
         print("starting login and join")
         login_and_join()
         break
     else:
         print("Meeting time not up. Sleeping for %s minutes" % str(sleep_time))
-        time.sleep(sleep_time*60)
+        time.sleep(sleep_time * 60)
